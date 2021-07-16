05e0a35009603f4428dedefd1035801bc4bebf8c
yuki <s1932058jl@s.chibakoudai.jp>
2021-07-17 08:10:18 +0900

    junitテスト

---
+++ junit-tutorial/test/junit/tutorial/CalculatorTest.java
@@ -0,0 +1,26 @@
+package junit.tutorial;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void maltiplicationで5と2の乗算結果が取得できる() throws Exception {
+		Calculator calc = new Calculator();
+		int expected = 10;
+		int actual = calc.maltiplication(5, 2);
+		assertThat(actual, is(expected));
+	}
+
+	@Test
+	public void divisionで5と8の除算結果が取得できる() throws Exception {
+		Calculator sut = new Calculator();
+		float expected = 0.625f;
+		float actual = sut.division(5, 8);
+		assertThat(actual, is(expected));
+	}
+
+}
